32434d6367dc37bbe138b9724511b47f1fc2cff5
hoangdx <hoangdangxuan098@gmial.com>
2024-03-27 10:47:09 +0700

    Coach_filters

---
+++ src/components/coaches/CoachFilter.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <base-card>
+      <h2>FInd your Coach</h2>
+      <span class="filter-option">
+        <input type="checkbox" id="frontend" checked @change="setFilter" />
+        <label for="frontend">Frontend</label>
+      </span>
+      <span class="filter-option">
+        <input type="checkbox" id="backend" checked @change="setFilter" />
+        <label for="backend">Backend</label>
+      </span>
+      <span class="filter-option">
+        <input type="checkbox" id="career" checked @change="setFilter" />
+        <label for="career">Career</label>
+      </span>
+    </base-card>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      filters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
+  },
+  emits: ['change-filter'],
+  methods: {
+    setFilter(event) {
+      const inputId = event.target.id;
+      const isActive = event.target.checked;
+      const updateFilters = {
+        ...this.filters,
+        [inputId]: isActive,
+      };
+      this.filters = updateFilters;
+      this.$emit('change-filter', updateFilters);
+    },
+  },
+};
+</script>
+<style scoped>
+h2 {
+  margin: 0.5rem 0;
+}
+
+.filter-option {
+  margin-right: 1rem;
+}
+
+.filter-option label,
+.filter-option input {
+  vertical-align: middle;
+}
+
+.filter-option label {
+  margin-left: 0.25rem;
+}
+
+.filter-option.active label {
+  font-weight: bold;
+}
+</style>
--- src/components/coaches/CoachItem.vue
+++ src/components/coaches/CoachItem.vue
@@ -3,15 +3,21 @@
     <h3>{{ fullName }}</h3>
     <h4>${{ rate }}/hour</h4>
     <div>
-      <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+      ></base-badge>
     </div>
     <div class="actions">
-      <base-button mode="outline" link :to="coachContactLink">Contact</base-button>
+      <base-button mode="outline" link :to="coachContactLink"
+        >Contact</base-button
+      >
       <base-button link :to="coachDetailsLink">View Details</base-button>
     </div>
   </li>
 </template>
-
 <script>
 export default {
   props: ['id', 'firstName', 'lastName', 'rate', 'areas'],
